bd6c725e3aeac2630348624a7462bbbed257ca83
Andrei <andrdru@gmail.com>
2016-12-01 00:40:05 +0300

    fix comment

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ $loader = pnf\loader::getInstance();
 //specify page title
 $loader->seo["title"] = "Main page title";
 
-//redefine some of $loader->views values,like this
-//$loader->views["main"] = "main.php"; //this will not take any effect, it's already defined
+//redefine some of $loader->modules values,like this
+//$loader->modules["main"] = "main.php"; //this will not take any effect, it's already defined
 
 //load views to template and show page
 $loader->printPage();
